3e3d9e770a4be4bdcb2ba67c399d6381303d129a
yango3 <o3.yang@hotmail.com>
2021-04-28 15:31:33 +0800

    u

--- out/hello.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Hello = void 0;
-function Hello() {
-    console.log("Hello friends.");
-}
-exports.Hello = Hello;
-//# sourceMappingURL=hello.js.map
--- out/test/testhello.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var index_1 = require("../index");
-console.log("test");
-index_1.Hello();
-//# sourceMappingURL=testhello.js.map
